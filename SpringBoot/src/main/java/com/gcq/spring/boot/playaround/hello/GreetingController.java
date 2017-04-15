23d99a34e990ee72936d407f733476b94d0eb114
gcq <gcqst@163.com>
2017-04-15 15:50:32 +0800

    add sub project of consumingRest and SpringJdbcTemplate. 2017-04-15 15:50

--- SpringBoot/src/main/java/com/gcq/spring/boot/playaround/hello/GreetingController.java
+++ SpringBoot/src/main/java/com/gcq/spring/boot/playaround/hello/GreetingController.java
@@ -1,5 +1,7 @@
 package com.gcq.spring.boot.playaround.hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +11,13 @@ import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class GreetingController {
+    private static final Logger log = LoggerFactory.getLogger(GreetingController.class);
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                String.format(template, name));
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        log.info("Get request for greeting");
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
